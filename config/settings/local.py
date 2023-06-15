1ee8dfacf7a0695150fb588d1a118d4936e242f0
mariaFernanda <maria.tejada@eyss.io>
2023-06-15 19:44:03 -0400

    agregando hosting

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="iDUHczAqTObNbHMDieFnDJsv2B15jNTQ2yR78jj2GuJJy46GsafLuiFuvrMHnDFU",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["65.108.86.238", "localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["65.21.110.188", "localhost", "0.0.0.0", "127.0.0.1"]
 
 # CACHES
 # ------------------------------------------------------------------------------
